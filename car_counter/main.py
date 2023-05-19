59a6f7c6f359e9e72d7b29f0738b1d89999f351a
infinity <priyanshu-hawk>
2023-05-19 20:30:18 +0530

    bug fix

--- car_counter/main.py
+++ car_counter/main.py
@@ -74,7 +74,7 @@ while True:
         cv2.rectangle(img, (max(0,x1), max(0,y1)), (x1+h, y1-w), (0,0,0), cv2.FILLED)
         cv2.putText(img, f'{id}', (max(0,x1), max(0,y1)), cv2.FONT_HERSHEY_PLAIN, 2, (0,0,255), 2)
 
-        cx, cy = x1+ch//2, y1+cw//2
+        cx, cy = x1+cw//2, y1+ch//2
         cv2.circle(img, (cx,cy), 5, (0,255,0), cv2.FILLED)
 
         if limits[0]<cx<limits[2] and limits[1]-15<cy<limits[3]+15:
